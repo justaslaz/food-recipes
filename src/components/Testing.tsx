e5c788d079853dc85093ca38505a45c91abb46ae
justaslaz <justas.lazevnikas@gmail.com>
2023-05-02 11:43:22 +0300

    feat: mockup data and some styling

---
+++ src/components/Testing.tsx
@@ -0,0 +1,34 @@
+const mockupData = [
+  {
+    name: 'Chicken breast',
+    id: 'someNiceId111',
+    uid: 'userCreatorId111',
+    image: 'url',
+    time: 30,
+    servingsDef: 2,
+    ingredients: [
+      {
+        name: 'rice',
+        quantity: 100,
+        unit: 'g',
+        quantitySecondary: 2,
+        unitSecondary: 'v.s.',
+      },
+      { name: 'chicken', quantity: 200, unit: 'g' },
+    ],
+    preparation: [
+      'Cut ...',
+      'Boil...',
+      'Cook...',
+      'Season...',
+      'Mix...',
+      'Pour...',
+      'Eat...',
+    ],
+    notes: ['Instead of ... you can use ...'],
+  },
+];
+
+export default function Testing() {
+  return <></>;
+}
