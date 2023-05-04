14fb3629242548d5b3ef0e3524fa0d428e900483
justaslaz <justas.lazevnikas@gmail.com>
2023-05-04 12:04:43 +0300

    feat: search button in nav

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Popover } from '@headlessui/react';
-import { Bars3Icon } from '@heroicons/react/24/outline';
+import { Bars3Icon, MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import logo from '../../../public/Chef_Outline.svg';
 import ReceptaiPopover from './ReceptaiPopover';
 
@@ -38,12 +38,19 @@ export default function Navigation() {
         </div>
 
         {/* Whole Navigation ex. Logo */}
-        <Popover.Group className="hidden lg:flex lg:gap-x-12">
+        <Popover.Group className="hidden lg:flex lg:items-center lg:justify-center lg:gap-x-12">
           <ReceptaiPopover />
-          {/* Search Pop-Up */}
+
+          {/* Search Button */}
+          <button className="flex w-full items-center justify-start gap-x-2 rounded-md border-0 bg-white py-1.5 pl-3 pr-24 text-sm leading-6 text-gray-400 ring-1 ring-inset ring-gray-300 hover:ring-green-500 active:ring-2">
+            <MagnifyingGlassIcon
+              className="h-5 w-5 text-gray-400"
+              aria-hidden="true"
+            />
+            <span>Ieškoti...</span>
+          </button>
 
           {/* TODO mock up below */}
-          <Link href="/">Search</Link>
           <Link href="/">My Favorites</Link>
           <Link href="/">Log In</Link>
         </Popover.Group>
