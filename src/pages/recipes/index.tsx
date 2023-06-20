7f0d5bb426f195b96c4cecdef8350da0bd33d974
justaslaz <justas.lazevnikas@gmail.com>
2023-06-20 13:19:25 +0300

    feat: add page scroll to top when changing pages

--- src/pages/recipes/index.tsx
+++ src/pages/recipes/index.tsx
@@ -7,7 +7,7 @@ import { api } from "~/utils/api";
 
 // Page
 const SearchResults: NextPage = () => {
-  const { query } = useRouter();
+  const { query, push } = useRouter();
   const [pageNum, setPageNum] = useState(1);
 
   const { data: recipesData, isLoading } = api.recipe.getByCategory.useQuery({
@@ -17,6 +17,8 @@ const SearchResults: NextPage = () => {
 
   if (isLoading) return <LoadingSpinner size="large" />;
   if (!recipesData) return <div>Oops, something went wrong!</div>;
+
+  const navigateTop = () => void push("/recipes");
 
   // Title
   const titleWords = query.categoryName
@@ -70,7 +72,10 @@ const SearchResults: NextPage = () => {
               {pageNum > 1 && (
                 <button
                   type="button"
-                  onClick={() => setPageNum((prev) => prev - 1)}
+                  onClick={() => {
+                    setPageNum((prev) => prev - 1);
+                    navigateTop();
+                  }}
                   className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-stone-900 ring-1 ring-inset ring-stone-300 hover:bg-stone-50 focus-visible:outline-offset-0"
                 >
                   Buvęs puslapis
@@ -79,7 +84,10 @@ const SearchResults: NextPage = () => {
               {pageNum * 9 < foundResultsAmount && (
                 <button
                   type="button"
-                  onClick={() => setPageNum((prev) => prev + 1)}
+                  onClick={() => {
+                    setPageNum((prev) => prev + 1);
+                    navigateTop();
+                  }}
                   className="ml-3 inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-stone-900 ring-1 ring-inset ring-stone-300 hover:bg-stone-50 focus-visible:outline-offset-0"
                 >
                   Kitas puslapis
