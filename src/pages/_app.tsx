c5bdf039a6905eaccf6603371cdfe7e9bafeaf9b
justaslaz <justas.lazevnikas@gmail.com>
2023-07-06 15:56:27 +0300

    fix: clerk auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
-import { type AppType } from "next/app";
-import { api } from "~/utils/api";
-import "~/styles/globals.css";
-import Layout from "~/components/Layout";
-import { Provider } from "jotai";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Provider } from "jotai";
+import { type AppType } from "next/app";
+import Layout from "~/components/Layout";
+import { clientEnv } from "~/env/schema.mjs";
+import "~/styles/globals.css";
+import { api } from "~/utils/api";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <ClerkProvider
+      publishableKey={clientEnv.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
       appearance={{
         layout: {
           socialButtonsPlacement: "bottom",
